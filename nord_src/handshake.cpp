d2d55df71981c099140cb95e321c08a8245bbece
jyhuang <jyhuang@tamu.edu>
2016-03-14 11:29:12 -0500

    Add handshake.hpp(cpp) to NoRD src

---
+++ nord_src/handshake.cpp
@@ -0,0 +1,81 @@
+// $Id: credit.cpp 5188 2012-08-30 00:31:31Z dub $
+
+/*
+ Copyright (c) 2007-2012, Trustees of The Leland Stanford Junior University
+ All rights reserved.
+
+ Redistribution and use in source and binary forms, with or without
+ modification, are permitted provided that the following conditions are met:
+
+ Redistributions of source code must retain the above copyright notice, this 
+ list of conditions and the following disclaimer.
+ Redistributions in binary form must reproduce the above copyright notice, this
+ list of conditions and the following disclaimer in the documentation and/or
+ other materials provided with the distribution.
+
+ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE 
+ DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+/*credit.cpp
+ *
+ *A class for credits
+ */
+
+#include "booksim.hpp"
+#include "handshake.hpp"
+#include "router.hpp"
+
+stack<Handshake *> Handshake::_all;
+stack<Handshake *> Handshake::_free;
+
+Handshake::Handshake()
+{
+  Reset();
+}
+
+void Handshake::Reset()
+{
+
+  power_gate = false;
+  wakeup = false;
+  incoming = false;
+  id = -1;
+}
+
+Handshake * Handshake::New() {
+  Handshake * hs;
+  if(_free.empty()) {
+    hs = new Handshake();
+    _all.push(hs);
+  } else {
+    hs = _free.top();
+    hs->Reset();
+    _free.pop();
+  }
+  return hs;
+}
+
+void Handshake::Free() {
+  _free.push(this);
+}
+
+void Handshake::FreeAll() {
+  while(!_all.empty()) {
+    delete _all.top();
+    _all.pop();
+  }
+}
+
+
+int Handshake::OutStanding(){
+  return _all.size()-_free.size();
+}
