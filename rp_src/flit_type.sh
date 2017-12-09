2e886a89aec0bd1e86feb0342e8b5683e42ff0e2
jyhuang <jyhuang@tamu.edu>
2017-12-09 13:46:25 -0600

    rm standalone rp_src and flov_src, been merged to unified framework in src

--- rp_src/flit_type.sh
+++
@@ -1,7 +0,0 @@
-./booksim meshconfig > a.txt
-grep "READ_REQUEST" ./a.txt > read_request.txt
-grep "WRITE_REQUEST" ./a.txt > write_request.txt
-grep "READ_REPLY" ./a.txt > read_reply.txt
-grep "WRITE_REPLY" ./a.txt > write_reply.txt
-wc -l read*
-wc -l write*
