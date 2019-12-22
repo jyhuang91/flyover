d8dcde07b6b77bd9af5a7ec6c4150f6d78b0ec93
jyhuang <jyhuang@tamu.edu>
2019-12-22 18:20:34 -0500

    [flov] dynamic power-gating parameters

--- src/flovtrafficmanager.hpp
+++ src/flovtrafficmanager.hpp
@@ -7,7 +7,7 @@
  Redistribution and use in source and binary forms, with or without
  modification, are permitted provided that the following conditions are met:
 
- Redistributions of source code must retain the above copyright notice, this 
+ Redistributions of source code must retain the above copyright notice, this
  list of conditions and the following disclaimer.
  Redistributions in binary form must reproduce the above copyright notice, this
  list of conditions and the following disclaimer in the documentation and/or
@@ -15,7 +15,7 @@
 
  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
  ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
- WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE 
+ WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
  DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
  ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
  (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
@@ -47,6 +47,12 @@ private:
   /* ==== Power Gate - Begin ==== */
   vector<Stats *> _flov_hop_stats;
   vector<double> _overall_flov_hop_stats;
+  double _plat_high_watermark;
+  double _plat_low_watermark;
+  int _monitor_counter;
+  int _monitor_epoch;
+  vector<Stats *> _per_node_plat;
+  string _powergate_type;
   /* ==== Power Gate - End ==== */
 
   /* ==== Power Gate - Begin ==== */
@@ -55,7 +61,7 @@ private:
   vector<vector<int> > _overall_idle_cycles;
   vector<bool> _wakeup_handshake_latency;
   /* ==== Power Gate - End ==== */
-  // ============ Internal methods ============ 
+  // ============ Internal methods ============
 protected:
 
   virtual void _RetireFlit( Flit *f, int dest );
@@ -72,7 +78,7 @@ protected:
 //  virtual bool _SingleSim( );
 
 //  void _DisplayRemaining( ostream & os = cout ) const;
-  
+
 //  void _LoadWatchList(const string & filename);
 
   virtual void _UpdateOverallStats();
@@ -83,7 +89,7 @@ protected:
 //  double _GetAveragePacketSize(int cl) const;
 
 public:
-  
+
   FLOVTrafficManager( const Configuration &config, const vector<Network *> & net );
   virtual ~FLOVTrafficManager( );
 
