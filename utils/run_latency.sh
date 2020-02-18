90a0b31d3080224da8e96ff62e4db2646edefcaa
jyhuang <jyhuang@tamu.edu>
2020-02-18 13:54:31 -0600

    [utils] run scripts for simulation and plot

--- utils/run_latency.sh
+++ utils/run_latency.sh
@@ -7,7 +7,7 @@ for traffic in uniform tornado
 do
   for inj in 0.02 0.08
   do
-    for scheme in baseline rpa rpc rflov gflov opt_rflov opt_gflov
+    for scheme in opt_flov #baseline rpa rpc nord flov opt_flov #rflov gflov opt_rflov opt_gflov
     do
       mkdir -p ../results/latency/${scheme}
       for off_percent in 10 20 30 40 50 60 70 80;
@@ -17,7 +17,14 @@ do
           count=`ps aux | grep booksim | wc -l`
           sleep 1
         done
-        ../src/booksim ../runfiles/${scheme}/meshcmp_${off_percent}off.cfg powergate_auto_config=${powergate_auto_config} traffic=${traffic} powergate_percentile=${off_percent} k=${dim} injection_rate=${inj} > ../results/latency/${scheme}/${traffic}_${inj}inj_${off_percent}off.log &
+        ../src/booksim \
+          ../runfiles/${scheme}/meshcmp_${off_percent}off.cfg \
+          powergate_auto_config=${powergate_auto_config} \
+          traffic=${traffic} \
+          powergate_percentile=${off_percent} \
+          k=${dim} \
+          injection_rate=${inj} \
+          > ../results/latency/${scheme}/${traffic}_${inj}inj_${off_percent}off.log &
       done
     done
   done
